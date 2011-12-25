ca5f6bce9ec9ed68b8fa76acbd883c3d0fc3459a
emile.contal <emile.contal@02d440ef-4846-32b8-4876-37cbc797d30e>
2011-12-25 23:29:28 +0000

    Lol, proper comments

--- dtree.ml
+++ dtree.ml
@@ -12,18 +12,18 @@ module Dtree =
 		let varSet_init l = 
 			List.fold_right VarSet.add l VarSet.empty
 			
-		"""
-		fun dtree -> varSet
-		variables of a dtree
-		"""
+		(**
+		 * fun dtree -> varSet
+		 * variables of a dtree
+		 *)
 		let rec vars = function
 			| Leaf(a,b) -> varSet_init [a;b]
       | Node(l, r) -> VarSet.union (vars l) ( vars r )
 
-		"""
-		fun graph -> dtree -> varSet
-		{ u | \exists (u,v) \in E s.t. v \notin vars G(t) }
-		"""
+		(**
+		 * fun graph -> dtree -> varSet
+		 * { u | \exists (u,v) \in E s.t. v \notin vars G(t) }
+	   *)
 		let context g t =
 			let vars_of_t = vars t in
 			let is_there_an_edge_outside g u =
@@ -33,10 +33,10 @@ module Dtree =
 				in
 			VarSet.filter (is_there_an_edge_outside g) vars_of_t
 			
-		"""
-		fun graph -> varSet -> varSet
-		vertices common to G(tl) and G(tr) that are not in the context of t
-		"""
+		(**
+		 * fun graph -> varSet -> varSet
+		 * vertices common to G(tl) and G(tr) that are not in the context of t
+		 *)
 		let cutset g ctxt = function
 			| Leaf(a,b) -> []
 			| Node(l,r) ->
